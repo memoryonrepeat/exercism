8fe12e93c926d48a4e7ed9ef55a16c57d387862f
Thanh Nguyen <thanh.nguyen@frontiercargroup.com>
2020-06-23 13:07:40 +0200

    resistor-color-duo

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,12 @@
 export class ResistorColor {
   private colors: string[];
+  private order = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
   constructor(colors: string[]) {
-    this.colors = colors;
+    if (colors.length < 2){
+      throw "At least two colors need to be present"
+    }
+    this.colors = colors.slice(0,2)
   }
-  value = (): number => 0;
+  value = (): number => parseInt(this.colors.reduce((acc, current) => (acc + this.order.indexOf(current)), ''))
 }
